1b3219a95761f36e7b63bbb8a396b1629c5ed444
marciszewskimarek <61851721+marciszewskimarek@users.noreply.github.com>
2023-02-28 22:03:11 +0000

    Corrents path.

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ for (const id of ids)
     // content.appendChild(btn);
     const video = document.createElement("video");
     video.id = id;
-    video.src = "/" + id + ".mov";
+    video.src = "happybirthdaybraktu/" + id + ".mov";
     video.muted = true;
     video.loop = true;
     video.play = true;
