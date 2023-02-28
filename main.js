7ae8dcfbd67c51c43619137ecaf2a51be167b59b
marciszewskimarek <61851721+marciszewskimarek@users.noreply.github.com>
2023-02-28 21:23:58 +0000

    Changes path.

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ for (const id of ids)
     // content.appendChild(btn);
     const video = document.createElement("video");
     video.id = id;
-    video.src = "./video/" + id + ".mov";
+    video.src = "/video/" + id + ".mov";
     video.muted = true;
     video.loop = true;
     video.play = true;
